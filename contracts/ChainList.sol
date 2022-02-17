298ea02c8d5ee30c3ef71b9757fd28877c8b1c07
Giuseppe Iacono <alchemist.bounty@protonmail.com>
2022-02-17 01:21:21 -0500

    ChainList and Migration

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -8,6 +8,18 @@ contract ChainList {
     uint256 price;
 
     function sellArticle(string _name, string _description, uint256 _price) public {
-        
+        seller = msg.sender;
+        name = _name;
+        description = _description;
+        price = _price;
+    }
+
+    function getArticle() public view returns (
+        address _seller, 
+        string _name, 
+        string _description, 
+        uint256 _price
+    ) {
+        return(seller, name, description, price);
     }
 }
