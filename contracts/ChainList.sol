771bf166fad7b2eb35e02043a0da85d878152797
Giuseppe Iacono <alchemist.bounty@protonmail.com>
2022-02-17 01:13:26 -0500

    Initial commit

---
+++ contracts/ChainList.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5;
+
+contract ChainList {
+    // state variables
+    address seller;
+    string name;
+    string description;
+    uint256 price;
+
+    function sellArticle(string _name, string _description, uint256 _price) public {
+        
+    }
+}
