e384bd860ee80e1f17753f65074999ce4d458626
Giuseppe Iacono <alchemist.bounty@protonmail.com>
2022-02-18 00:46:48 -0500

    Test sell

--- test/ChainListHappyPath.js
+++ test/ChainListHappyPath.js
@@ -1,6 +1,12 @@
 var ChainList = artifacts.require("./ChainList.sol");
 
 contract('ChainList', function(accounts) {
+    var chainListInstance;
+    var seller = accounts[1];
+    var articleName = "article 1";
+    var articleDescription = "Description for article 1";
+    var articlePrice = "10";
+
     it("should be initialized with empty values", function() {
         return ChainList.deployed().then(function(instance) {
             return instance.getArticle();
@@ -10,6 +16,23 @@ contract('ChainList', function(accounts) {
             assert.equal(data[2], '', 'Article description must be empty');
             assert.equal(data[3].toNumber(), 0, 'Article Price must be empty');
         });
-        
     });
+
+    it("should sell an article", function() {
+        return ChainList.deployed().then(function(instance) {
+            chainListInstance = instance;
+            return chainListInstance.sellArticle(
+                articleName, 
+                articleDescription, 
+                web3.utils.toWei(articlePrice, "ether"),
+                {from: seller});
+        }).then(function() {
+            return chainListInstance.getArticle();
+        }).then(function(data) {
+            assert.equal(data[0], seller, 'seller must be ' + seller);
+            assert.equal(data[1], articleName, 'Article name must be ' + articleName);
+            assert.equal(data[2], articleDescription, 'Article description must be ' + articleDescription);
+            assert.equal(data[3], web3.utils.toWei(articlePrice, "ether"), 'Article Price must be ' + web3.utils.toWei(articlePrice, "ether"));
+        })
+    })
 })
